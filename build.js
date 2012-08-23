50bf27fc2067a99b21d804c80df5ee8233024ee6
cadorn <christoph@christophdorn.com>
2012-08-23 10:18:00 -0700

    Revert "Update build script"
    This reverts commit 6c35690ce9480f8b9d51c61a8c54ba7dbc1112af.

--- build.js
+++ build.js
@@ -14,15 +14,13 @@ var sm = new Smasher({
 panda.make(["./manifest.json", "-t", "Cloud9 Documentation", "--assets", "./assets", development ? "-r" : null], function(err, cbReturn) {
     if (err) console.error(err);
     
-    if (development) {
-      console.log("Compiling assets...");
-       sm.compileAssets(function(err) {
-         if(err) {
-           console.error("An asset compilation error occurred", err);
-         } else {
-           console.log("Compilation done!");
-         }
-         return;
-       });
-     }
+    console.log("Compiling assets...");
+     sm.compileAssets(function(err) {
+       if(err) {
+         console.error("An asset compilation error occurred", err);
+       } else {
+         console.log("Compilation done!");
+       }
+       return;
+     });
 });
